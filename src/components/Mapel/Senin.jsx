b60efe73f75b2e2c504337ae531698a3dc2312f2
Lyntharx <lumnztyz@gmail.com>
2026-03-05 05:08:43 +0700

    Fix JSX syntax for 'Mtk' div element

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -23,7 +23,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm"></div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium"Mtk</div>
+					<div className="w-[50%] text-base font-medium">Mtk</div>
 					<div className="flex justify-center items-center text-sm"></div>
 				</div>
         </>
